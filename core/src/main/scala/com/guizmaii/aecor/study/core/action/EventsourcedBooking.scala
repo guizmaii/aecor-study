d94de1bfda81b767a5bed2bf301cca46b7e8afc3
jules Ivanic <jules.ivanic@gmail.com>
2018-12-25 16:22:42 +0100

    Clean

--- core/src/main/scala/com/guizmaii/aecor/study/core/action/EventsourcedBooking.scala
+++ core/src/main/scala/com/guizmaii/aecor/study/core/action/EventsourcedBooking.scala
@@ -9,16 +9,15 @@ class EventsourcedBooking[F[_]](
     implicit F: MonadActionReject[F, Option[BookingState], BookingEvent, BookingCommandRejection]
 ) extends Booking[F] {
 
-  import F._
   import cats.syntax.all._
 
   override def place(client: ClientId, concert: ConcertId, seats: NonEmptyList[Seat]): F[Unit] =
-    read.flatMap {
-      case Some(_) => reject(BookingAlreadyExists)
+    F.read.flatMap {
+      case Some(_) => F.reject(BookingAlreadyExists)
       case None =>
-        if (seats.distinct =!= seats) reject(DuplicateSeats)
-        else if (seats.size > 10) reject(TooManySeats)
-        else append(BookingPlaced(client, concert, seats))
+        if (seats.distinct =!= seats) F.reject(DuplicateSeats)
+        else if (seats.size > 10) F.reject(TooManySeats)
+        else F.append(BookingPlaced(client, concert, seats))
     }
 
   override def deny(reason: String): F[Unit]                   = ???
